87b573e7de834a69a197e2446710d7e3fd7f1877
ravindukariyapperuma <ravindu.kariyapperuma@gmail.com>
2020-06-23 00:36:54 +0530

    there is no I in team

--- functions.test.js
+++ functions.test.js
@@ -33,5 +33,10 @@ test('Should be under 1600', () => {
     const load1 = 800;
     const load2 = 800;
     expect(load1 + load2).toBeLessThanOrEqual(1600);
-})
+});
+
+// Regex
+test('There is no I in team', () => {
+    expect('team').not.toMatch(/I/);
+});
 
