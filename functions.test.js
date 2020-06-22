2003de3feccdf7c4902833688e7a4e180c59a4b8
ravindukariyapperuma <ravindu.kariyapperuma@gmail.com>
2020-06-23 00:32:02 +0530

    should be under 1600

--- functions.test.js
+++ functions.test.js
@@ -20,10 +20,18 @@ test('Should be falsy', () => {
     expect(functions.cheackValue(undefined)).toBeFalsy();
 });
 
+// toEqual
 test('User should be Brad Traversy object', () => {
     expect(functions.createUser()).toEqual({
         firstName: 'Brad',
         lastName: 'Traversy'
     });
 });
+
+// Less than and greater than
+test('Should be under 1600', () => {
+    const load1 = 800;
+    const load2 = 700;
+    expect(load1 + load2).toBeLessThan(1600);
+})
 
