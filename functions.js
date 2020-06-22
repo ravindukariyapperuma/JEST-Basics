d841ddd6e5811dbb9df681b83c9466f814d76ac3
ravindukariyapperuma <ravindu.kariyapperuma@gmail.com>
2020-06-23 00:10:28 +0530

    user should be brad traversy object

--- functions.js
+++ functions.js
@@ -1,7 +1,12 @@
 const functions = {
     add: (num1, num2) => num1 + num2,
     isNull: () => null,
-    cheackValue: (x) => x
+    cheackValue: (x) => x,
+    createUser: () => {
+        const user = { firstName: 'Brad' }
+        user['lastName'] = 'Traversy';
+        return user;
+    }
 }
 
 module.exports = functions;
--- functions.test.js
+++ functions.test.js
@@ -1,18 +1,29 @@
 const functions = require('./functions');
 
+//toBe
 test('Adds 2 + 2 to equal 4', () => {
     expect(functions.add(2, 2)).toBe(4);
 });
 
+// not
 test('Adds 2 + 2 to NOT equal 5', () => {
     expect(functions.add(2, 2)).not.toBe(5);
 });
 
+// toBeNull
 test('Should be null', () => {
     expect(functions.isNull()).toBeNull();
 });
 
+// toBeFalsy
 test('Should be falsy', () => {
-    expect(functions.cheackValue(2)).toBeFalsy();
+    expect(functions.cheackValue(undefined)).toBeFalsy();
+});
+
+test('User should be Brad Traversy object', () => {
+    expect(functions.createUser()).toBe({
+        firstName: 'Brad',
+        lastName: 'Traversy'
+    });
 });
 
