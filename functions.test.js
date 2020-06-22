9ef394ca8fa42c0f5483060510fcf85ff12f9c21
ravindukariyapperuma <ravindu.kariyapperuma@gmail.com>
2020-06-22 22:45:48 +0530

    should be null

--- functions.test.js
+++ functions.test.js
@@ -7,3 +7,7 @@ test('Adds 2 + 2 to equal 4', () => {
 test('Adds 2 + 2 to NOT equal 5', () => {
     expect(functions.add(2, 2)).not.toBe(5);
 });
+
+test('Should be null', () => {
+    expect(functions.isNull()).toBeNull();
+});
